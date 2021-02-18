fb28c1c9f199b877da292d0008926ecee48d80d9
Mayank Bhatt <mayank.bhatt@magicsw.com>
2021-02-18 23:39:34 +0530

    setting up redux and cart design changed

--- frontend/src/Screens/CartScreen.js
+++ frontend/src/Screens/CartScreen.js
@@ -1,9 +1,27 @@
 import './CartScreen.css';
 
+// Components
+import CartItem from '../components/CartItem'
+
 function CartScreen() {
   return (
-    <div className="cart-screen">
-
+    <div className="cartscreen">
+      <div className="cartscreen__left">
+        <h2>Shopping Cart</h2>
+        <CartItem />
+        <CartItem />
+        <CartItem />
+        <CartItem />
+      </div>
+      <div className="cartscreen__right">
+        <div className="cartscreen__info">
+          <p>Subtotal (0) items</p>
+          <p>$499.9</p>
+        </div>
+        <div>
+          <button>Proceed To Checkout</button>
+        </div>
+      </div>
     </div>
   );
 }
